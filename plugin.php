a60d278154ce77117eaa91fc11aebdce3e35e519
Maximilian Sedlmaier <m.sedlmaier@datam.de>
2013-08-23 13:00:09 +0200

    generates now a valid url format

--- plugin.php
+++ plugin.php
@@ -19,14 +19,14 @@ class KokenPiwikAnalytics extends KokenPlugin {
   _paq.push(["enableLinkTracking"]);
 
   (function() {
-    var u=(("https:" == document.location.protocol) ? "https" : "http") + "{$this->data->tracking_url}/";
+    var u=(("https:" == document.location.protocol) ? "https" : "http") + "://{$this->data->tracking_url}/";
     _paq.push(["setTrackerUrl", u+"piwik.php"]);
     _paq.push(["setSiteId", "{$this->data->site_id}"]);
     var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
     g.defer=true; g.async=true; g.src=u+"piwik.js"; s.parentNode.insertBefore(g,s);
   })();
 </script>
-<noscript><img src="{$this->data->tracking_url}/piwik.php?idsite={$this->data->site_id}&amp;rec=1" style="border:0" alt="" /></noscript>
+<noscript><img src="http://{$this->data->tracking_url}/piwik.php?idsite={$this->data->site_id}&amp;rec=1" style="border:0" alt="" /></noscript>
 <!-- End Piwik Code -->
 OUT;
 
